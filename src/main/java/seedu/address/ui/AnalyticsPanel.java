b1dfafd53df46728c753df09592559a1496f9eb9
marcus-ny <kyalsin321@gmail.com>
2024-03-30 20:12:32 +0800

    Create AnalyticsPanel.java

---
+++ src/main/java/seedu/address/ui/AnalyticsPanel.java
@@ -0,0 +1,35 @@
+package seedu.address.ui;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
+import javafx.scene.layout.Region;
+import seedu.address.model.person.Analytics;
+
+public class AnalyticsPanel extends UiPart<Region> {
+    private static final String FXML = "AnalyticsPanel.fxml";
+
+    @FXML
+    private PieChart pieChart;
+
+    /**
+     * Creates a {@code AnalyticsPanel} with the given {@code ObjectProperty}.
+     */
+    public AnalyticsPanel(ObjectProperty<Analytics> analytics) {
+        super(FXML);
+        analytics.addListener((observable, oldValue, newValue) -> {
+            updateChart(newValue);
+        });
+    }
+
+    private void updateChart(Analytics analytics) {
+        pieChart.getData().clear();
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+                new PieChart.Data("Overdue", analytics.getNumOverdueLoans()),
+                new PieChart.Data("Not Overdue", analytics.getNumLoans() - analytics.getNumOverdueLoans())
+        );
+        pieChart.setData(pieChartData);
+    }
+}
