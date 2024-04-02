970371b9d89af80758bb6e37a0afb69c1d355f3b
marcus-ny <kyalsin321@gmail.com>
2024-04-03 02:44:50 +0800

    Implement GUI for analytics dashboard

--- src/main/java/seedu/address/ui/AnalyticsPanel.java
+++ src/main/java/seedu/address/ui/AnalyticsPanel.java
@@ -9,6 +9,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import seedu.address.model.analytics.DashboardData;
 import seedu.address.model.person.Analytics;
@@ -29,6 +30,15 @@ public class AnalyticsPanel extends UiPart<Region> {
     @FXML
     private PieChart urgencyChart;
 
+    @FXML
+    private Label reliabilityIndex;
+
+    @FXML
+    private Label impactIndex;
+
+    @FXML
+    private Label urgencyIndex;
+
     private static final float MAX_IMPACT = 1000000;
 
     /**
@@ -36,30 +46,47 @@ public class AnalyticsPanel extends UiPart<Region> {
      */
     public AnalyticsPanel(ObjectProperty<DashboardData> dashboardData) {
         super(FXML);
+        initializeCharts();
         reliabilityChart.setData(FXCollections.observableArrayList());
         dashboardData.addListener((observable, oldValue, newValue) -> {
             updateChart(newValue);
         });
     }
 
+    private void initializeCharts() {
+        reliabilityChart.setStartAngle(90);
+        impactChart.setStartAngle(90);
+        urgencyChart.setStartAngle(90);
+        reliabilityChart.setLabelsVisible(false);
+        impactChart.setLabelsVisible(false);
+        urgencyChart.setLabelsVisible(false);
+        reliabilityChart.setLegendVisible(false);
+        impactChart.setLegendVisible(false);
+        urgencyChart.setLegendVisible(false);
+    }
+
     private void updateChart(DashboardData data) {
         Analytics analytics = data.getAnalytics();
         ObservableList<PieChart.Data> reliabilityData = FXCollections.observableArrayList(
-                new PieChart.Data("Active Loans", analytics.getNumActiveLoans()),
-                new PieChart.Data("Overdue Loans", analytics.getNumActiveLoans() - analytics.getNumOverdueLoans())
+                new PieChart.Data("Reliability Index", analytics.getPropOverdueLoans()),
+                new PieChart.Data("", 1 - analytics.getPropOverdueLoans())
         );
         reliabilityChart.setData(reliabilityData);
 
         ObservableList<PieChart.Data> impactData = FXCollections.observableArrayList(
                 new PieChart.Data("Impact Index", data.getImpactIndex()),
-                new PieChart.Data("Benchmark", 1 - data.getImpactIndex())
+                new PieChart.Data("", 1 - data.getImpactIndex())
         );
         impactChart.setData(impactData);
 
         ObservableList<PieChart.Data> urgencyData = FXCollections.observableArrayList(
                 new PieChart.Data("Urgency Index", data.getUrgencyIndex()),
-                new PieChart.Data("Benchmark", 1 - data.getUrgencyIndex())
+                new PieChart.Data("", 1 - data.getUrgencyIndex())
         );
         urgencyChart.setData(urgencyData);
+
+        reliabilityIndex.setText(String.format("%.2f", (1 - analytics.getPropOverdueLoans()) * 100) + "%");
+        impactIndex.setText(String.format("%.2f", data.getImpactIndex() * 100) + "%");
+        urgencyIndex.setText(String.format("%.2f", data.getUrgencyIndex() * 100) + "%");
     }
 }
